cfd50f3e550450fe771d1cb68958891a372d3a85
Rérisson Fumes <rkfumes@gmail.com>
2022-12-18 11:44:56 -0300

    Ajustes hostinger

--- backend/src/configs/database.config.js
+++ backend/src/configs/database.config.js
@@ -6,6 +6,7 @@ module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   charset: 'utf8',
   collate: 'utf8_general_ci',
   freezeTableNames: true,
